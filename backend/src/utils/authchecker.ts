ea0418897967741c610f42db4c7613441d111c2f
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2023-04-22 16:11:47 +0530

    added role based auth

--- backend/src/utils/authchecker.ts
+++ backend/src/utils/authchecker.ts
@@ -2,8 +2,13 @@ import MyContext from "./context";
 
 import { AuthChecker } from "type-graphql";
 
-const authChecker: AuthChecker<MyContext> = async ({ context: { user } }) => {
+const authChecker: AuthChecker<MyContext> = async (
+  { context: { user } },
+  roles
+) => {
   if (!user) return false;
+  if (!roles || roles.length == 0) return true;
+  if (!roles.includes(user.role)) return false;
   return true;
 };
 
