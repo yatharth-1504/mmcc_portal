50563d75457bd52b4e2eca62f94cc63439a77c7c
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2023-04-22 16:12:04 +0530

    added dev list

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,2 +1,7 @@
 export const emailExpresion =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const usersDevList = [
+  { roll: "BS20B039", name: "Yatharth", pass: "123456" },
+  { roll: "CH20B010", name: "Aman Kulwal", pass: "123456" },
+];
