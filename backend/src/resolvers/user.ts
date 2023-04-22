30ffb14d9fcc490e3ca71815d136b71a33483cbc
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2023-04-22 16:11:03 +0530

    added addTestUserMutation

--- backend/src/resolvers/user.ts
+++ backend/src/resolvers/user.ts
@@ -5,6 +5,8 @@ import LoginOutput from "../types/objects/user";
 import MyContext from "../utils/context";
 import { LoginInput } from "../types/inputs/user";
 import jwt from "jsonwebtoken";
+import { usersDevList } from "../utils";
+import { UserRole } from "../types/enums/user";
 
 @Resolver(() => User)
 class UserResolver {
@@ -12,11 +14,36 @@ class UserResolver {
   async login(@Arg("login") { roll, password }: LoginInput) {
     try {
       const user = await User.findOne({ where: { roll: roll } });
+      console.log("Here I am!");
       if (!user) throw new Error("Invalid Email");
       const passwordIsValid = bcryptjs.compareSync(password, user.password);
       if (!passwordIsValid) throw new Error("Invalid Credentials");
       let token = jwt.sign(user.id, process.env.JWT_SECRET!);
       return { token: token, status: true };
+    } catch (e) {
+      throw new Error(`error : ${e}`);
+    }
+  }
+
+  @Mutation(() => Boolean)
+  // @Authorized([UserRole.ADMIN])
+  async addTestUsers() {
+    try {
+      if (process.env.NODE_ENV !== "production") {
+        const users = await Promise.all(
+          usersDevList.map(
+            async (_user) =>
+              await User.create({
+                roll: _user.roll,
+                password: _user.pass,
+                name: _user.name,
+                role: UserRole.ADMIN,
+              }).save()
+          )
+        );
+        if (!!users) return true;
+      }
+      return false;
     } catch (e) {
       throw new Error(`error : ${e}`);
     }
