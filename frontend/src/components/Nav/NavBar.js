58bdb2ed46526d42cf15aee99e6be2d33f1d93d5
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2023-04-28 22:37:29 +0530

    added Nav Comp

---
+++ frontend/src/components/Nav/NavBar.js
@@ -0,0 +1,25 @@
+import add_img from "../../assets/add-button.jpg";
+import "./NavBar.scss";
+
+export function NavBar({ buttons }) {
+  return (
+    <div>
+      <nav className="Nav">
+        {buttons.map((button) => (
+          <div className="filters_sort">
+            {button.name + ":"}
+            <select className="dropdown ">
+              {button.conditions.map((c) => (
+                <option>{c}</option>
+              ))}
+            </select>
+          </div>
+        ))}
+        <button>
+          <img className="img" src={add_img}></img>
+          {"Add Compliant"}
+        </button>
+      </nav>
+    </div>
+  );
+}
