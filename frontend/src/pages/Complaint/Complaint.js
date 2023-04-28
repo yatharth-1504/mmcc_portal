dc77aa2597cd12d3314c924dae4ad811ddb3ff96
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2023-04-28 22:41:13 +0530

    added complaint page

---
+++ frontend/src/pages/Complaint/Complaint.js
@@ -0,0 +1,32 @@
+import { Preview } from "../../components/Preview/Preview";
+import { useFetch } from "../../hooks/fetch";
+import { NavBar } from "../../components/Nav/NavBar";
+import "./Complaint.scss";
+
+export function Complaint() {
+  const buttons = [
+    {
+      name: "Filters",
+      conditions: ["All Complaints", "CMFGS", "MMCC"],
+    },
+    {
+      name: "Sort By",
+      conditions: ["Date(newest first)", "Date(oldest first)"],
+    },
+  ];
+
+  const { complaints, isPending, errors } = useFetch(
+    "http://localhost:3001/complaints"
+  );
+
+  return (
+    <div className="Complaints">
+      <NavBar buttons={buttons} />
+      {isPending && <div className="Loading">Loading ...</div>}
+      {errors && (
+        <div className="Error">Errors in fetching the resource... :(</div>
+      )}
+      {!!complaints && <Preview complaints={complaints} />}
+    </div>
+  );
+}
