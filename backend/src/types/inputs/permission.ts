a52f03afdd56509c3bd86f487bd23cf38cf0f76c
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2023-05-19 01:25:54 +0530

    edited formating

--- backend/src/types/inputs/permission.ts
+++ backend/src/types/inputs/permission.ts
@@ -1,16 +1,16 @@
-import {Field, InputType} from "type-graphql";
-import {UserRole} from "../enums/user";
+import { Field, InputType } from "type-graphql";
+import { UserRole } from "../enums/user";
 
 @InputType()
 class CreatePermissionInput {
-    @Field(() => [UserRole])
-    updateRoleTo: UserRole[];
+  @Field(() => [UserRole])
+  updateRoleTo: UserRole[];
 
-    @Field(() => [UserRole])
-    updateRoleOf: UserRole[];
+  @Field(() => [UserRole])
+  updateRoleOf: UserRole[];
 
-    @Field(() => [UserRole])
-    assignComplaintsTo: UserRole[];
+  @Field(() => [UserRole])
+  assignComplaintsTo: UserRole[];
 }
 
-export {CreatePermissionInput};
+export { CreatePermissionInput };
